d22fea468957cc0b213918c40d6a03b2556f2214
phosseini <pdr.hosseini@gmail.com>
2024-11-11 19:31:02 -0800

    adding requirements.txt + renaming arguments

--- src/summarize_pdfs.py
+++ src/summarize_pdfs.py
@@ -5,7 +5,7 @@ from anthropic_api import AnthropicAPI
 from pathlib import Path
 
 
-def summarize_pdfs_in_directory(input_dir, prompt_path, output_file):
+def summarize_pdfs_in_directory(pdfs_dir, prompt_path, output_path):
     """
     Summarizes all PDF files in a given directory and saves the results to a JSON file.
 
@@ -21,7 +21,7 @@ def summarize_pdfs_in_directory(input_dir, prompt_path, output_file):
     summaries = {}
 
     # Process each PDF file in the directory
-    for pdf_file in Path(input_dir).glob("*.pdf"):
+    for pdf_file in Path(pdfs_dir).glob("*.pdf"):
         summary = api_client.summarize_pdf(str(pdf_file), prompt_path)
 
         if summary:
@@ -32,20 +32,20 @@ def summarize_pdfs_in_directory(input_dir, prompt_path, output_file):
             print(f"Failed to summarize: {pdf_file.name}")
 
     # Save the summaries to a JSON file
-    with open(output_file, 'w', encoding='utf-8') as json_file:
+    with open(output_path, 'w', encoding='utf-8') as json_file:
         json.dump(summaries, json_file, ensure_ascii=False, indent=4)
 
-    print(f"Summaries saved to {output_file}")
+    print(f"Summaries saved to {output_path}")
 
 
 if __name__ == "__main__":
     # Argument parser setup
     parser = argparse.ArgumentParser(description="Summarize a PDF using Anthropic API")
-    parser.add_argument('--pdf-path', type=str, required=True, help="Path to the base64 encoded PDF file")
+    parser.add_argument('--pdfs-dir', type=str, required=True, help="Path to the PDFs directory")
     parser.add_argument('--prompt-path', type=str, required=True, help="Path to the .txt file containing the prompt")
-    parser.add_argument('--output-file', type=str, required=True, help="Path to the .json file to store the output",
+    parser.add_argument('--output-path', type=str, help="Path to the .json file to store the output",
                         default="../data/result.json")
 
     args = parser.parse_args()
 
-    summarize_pdfs_in_directory(args.pdf_path, args.prompt_path, args.output_file)
+    summarize_pdfs_in_directory(args.pdfs_dir, args.prompt_path, args.output_path)
